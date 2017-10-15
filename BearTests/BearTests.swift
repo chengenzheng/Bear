225158b6711603571af74c1ca29009142cf5ca1e
chengen Zheng <chengenzheng@chengendemacbook-pro.local>
2017-10-14 20:35:26 -0500

    Initial Commit

---
+++ BearTests/BearTests.swift
@@ -0,0 +1,36 @@
+//
+//  BearTests.swift
+//  BearTests
+//
+//  Created by chengen Zheng on 2017/10/14.
+//  Copyright © 2017年 chengen Zheng. All rights reserved.
+//
+
+import XCTest
+@testable import Bear
+
+class BearTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
