366f5d067938e0e0621c696a1400fd55bba41244
chengenzheng <32253758+chengenzheng@users.noreply.github.com>
2017-11-06 23:52:41 -0600

    week 8

---
+++ CustomControls的副本/CustomButton.swift
@@ -0,0 +1,44 @@
+//
+//  CustomButton.swift
+//  geoMessenger
+//
+//  Created by chengen Zheng on 2017/10/29.
+//  Copyright © 2017年 chengen Zheng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class CustomButton: UIButton {
+    @IBInspectable var borderColor: UIColor? = UIColor.clear {
+        didSet {
+            layer.borderColor = self.borderColor?.cgColor
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = self.borderWidth
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = self.cornerRadius
+            layer.masksToBounds = self.cornerRadius > 0
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        self.layer.cornerRadius = self.cornerRadius
+        self.layer.borderWidth = self.borderWidth
+        self.layer.borderColor = self.borderColor?.cgColor
+    }
+}
